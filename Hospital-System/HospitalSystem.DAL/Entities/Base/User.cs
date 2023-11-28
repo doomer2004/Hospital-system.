0ca13c95c469f43b0c1e2228510e97f4c084796b
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-11-28 19:50:12 +0200

    User role add

---
+++ Hospital-System/HospitalSystem.DAL/Entities/Base/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HospitalSystem.DAL.Entities.Base;
+
+public class User : IdentityUser<Guid>
+{
+    public string Name { get; set; }
+    public string? RefrashToken { get; set; }
+    public DateTimeOffset RefrashTokenExpiresAt { get; set; }
+}
