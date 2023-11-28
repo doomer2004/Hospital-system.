0e4016498bfa859faa155f9239560c3b2d5a8341
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-11-28 19:50:37 +0200

    DB Context add

---
+++ Hospital-System/HospitalSystem.DAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HospitalSystem.DAL.Entities;
+using HospitalSystem.DAL.Entities.Base;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalSystem.DAL.Context;
+
+public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
+    public virtual DbSet<User> Users { get; set; } = null!;
+    public virtual DbSet<Doctor> Doctors { get; set; } = null!;
+    public virtual DbSet<DoctorsSpecialty> DoctorsSpecialty { get; set; } = null!;
+    public virtual DbSet<Specialty> Specialty { get; set; } = null!;
+    public virtual DbSet<Patient> Patients { get; set; } = null!;
+    public virtual DbSet<DoctorTimeSlots> DoctorTimeSlots { get; set; } = null!;
+    public virtual DbSet<Appointment> Appointments { get; set; } = null!;
+}
