6826d9355f36bcf0c39b3f6de20421dfab0cf9e4
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-12-12 23:50:30 +0200

    Create DoctorsSpecialty.cs

---
+++ Hospital-System/HospitalSystem.Common/Enum/DoctorsSpecialty.cs
@@ -0,0 +1,12 @@
+﻿namespace HospitalSystem.Common.Enum;
+
+public enum DoctorsSpecialty
+{
+    Allergist = 1,
+    Cardiologist ,
+    Dermatologist ,
+    Gastroenterologist,
+    Oncologist,
+    Ophthalmologist,
+    
+}
