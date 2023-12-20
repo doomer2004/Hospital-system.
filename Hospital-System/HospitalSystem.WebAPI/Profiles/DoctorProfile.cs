aab251684bd41d9492974a97b41400a8c7123a63
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-12-20 16:39:18 +0200

    Profiles

---
+++ Hospital-System/HospitalSystem.WebAPI/Profiles/DoctorProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HospitalSystem.Common.Models.DTOs.Doctor;
+using HospitalSystem.DAL;
+using HospitalSystem.DAL.Entities;
+
+namespace Hospital_System.Profiles;
+
+public class DoctorProfile : Profile
+{
+    public DoctorProfile()
+    {
+        CreateMap<CreateDoctorDTO, Doctor>();
+        CreateMap<DoctorDTO, Doctor>()
+            .ForMember(dest => dest.StartTime, opt => opt.MapFrom(src => src.StartTime))
+            .ForMember(dest => dest.EndTime, opt => opt.MapFrom(src => src.EndTime));
+    }
+}
---
+++ Hospital-System/HospitalSystem.WebAPI/Profiles/SpecialtyProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HospitalSystem.Common.Models.DTOs.Doctor;
+using HospitalSystem.DAL;
+
+namespace Hospital_System.Profiles;
+
+public class SpecialtyProfile : Profile
+{
+    public SpecialtyProfile()
+    {
+        CreateMap<SpecialtyDTO, Specialty>();
+    }
+}
