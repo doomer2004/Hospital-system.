0f164238abc12bfe15248d523a07239c20c84ac7
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-12-20 16:36:16 +0200

    Controllers add

---
+++ Hospital-System/HospitalSystem.WebAPI/Controllers/DoctorTimeSlotsController.cs
@@ -0,0 +1,44 @@
+﻿using HospitalSystem.BLL.Services.Appointment.Interfaces;
+using HospitalSystem.Common.Constants;
+using HospitalSystem.Common.Models.DTOs.Doctor;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_System.Controllers;
+
+
+
+[ApiController]
+[Route("api/time-slots")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class DoctorTimeSlotsController : ControllerBase
+{
+    private readonly IDoctorTimeSlotsService _doctorTimeSlotsService;
+
+    public DoctorTimeSlotsController(IDoctorTimeSlotsService doctorTimeSlotsService)
+    {
+        _doctorTimeSlotsService = doctorTimeSlotsService;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = ApplicationRoles.Patient)]
+    public async Task<IActionResult> CreateDoctorTimeSlotsAsync(DoctorTimeSlotsDTO doctorTimeSlots, DateTime startsAt, DateTime endsAt)
+    {
+        return Ok(await _doctorTimeSlotsService.CreateAppointmentAsync(doctorTimeSlots, startsAt, endsAt));
+    }
+
+    [HttpDelete]
+    [Authorize(Roles = ApplicationRoles.Patient)]
+    public async Task<IActionResult> DeleteDoctorTimeSlotsAsync(Guid id)
+    {
+        return Ok(await _doctorTimeSlotsService.DeleteAppointmentAsync(id));
+    }
+
+    [HttpPut]
+    [Authorize(Roles = ApplicationRoles.Patient)]
+    public async Task<IActionResult> UpdateDoctorTimeSlotsAsync(Guid id, DateTime startsAt, DateTime endsAt)
+    {
+        return Ok(await _doctorTimeSlotsService.UpdateAppointmentAsync(id, startsAt, endsAt));
+    }
+}
---
+++ Hospital-System/HospitalSystem.WebAPI/Controllers/DoctorsSpecialtyController.cs
@@ -0,0 +1,58 @@
+﻿using HospitalSystem.BLL.Services.Doctor.Interfaces;
+using HospitalSystem.Common.Constants;
+using HospitalSystem.Common.Models.DTOs.Doctor;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_System.Controllers;
+
+
+[ApiController]
+[Route("api/specialties")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class DoctorsSpecialtyController : ControllerBase
+{
+    private readonly IDoctorsSpecialtyService _doctorsSpecialtyService;
+
+
+    public DoctorsSpecialtyController(IDoctorsSpecialtyService doctorsSpecialtyService)
+    {
+        _doctorsSpecialtyService = doctorsSpecialtyService;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = ApplicationRoles.Staff)]
+    public async Task<IActionResult> CreateDoctorsSpecialtyAsync(SpecialtyDTO specialty)
+    {
+        return Ok(await _doctorsSpecialtyService.CreateDoctorsSpecialtyAsync(specialty));
+    }
+
+    [HttpDelete]
+    [Authorize(Roles = ApplicationRoles.Staff)]
+    public async Task<IActionResult> DeleteDoctorsSpecialtyAsync(Guid id)
+    {
+        return Ok(await _doctorsSpecialtyService.DeleteDoctorsSpecialtyAsync(id));
+    }
+
+    [HttpGet("{id}")]
+    [Authorize(Roles = ApplicationRoles.Staff)]
+    public async Task<IActionResult> GetDoctorsSpecialtyByIdAsync(Guid id)
+    {
+        return Ok(await _doctorsSpecialtyService.GetDoctorsSpecialtyByIdAsync(id));
+    }
+
+    [HttpGet("by-doctor/{doctorId}")]
+    [Authorize(Roles = ApplicationRoles.Staff)]
+    public async Task<IActionResult> GetDoctorsSpecialtyByDoctorAsync(DoctorDTO doctor)
+    {
+        return Ok(await _doctorsSpecialtyService.GetDoctorsSpecialtyByDoctorAsync(doctor));
+    }
+
+    [HttpPut]
+    [Authorize(Roles = ApplicationRoles.Staff)]
+    public async Task<IActionResult> UpdateDoctorsSpecialtyAsync(SpecialtyDTO specialty)
+    {
+        return Ok(await _doctorsSpecialtyService.UpdateDoctorsSpecialtyAsync(specialty));
+    }
+}
